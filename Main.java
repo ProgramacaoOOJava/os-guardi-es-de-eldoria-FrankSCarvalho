da565d40da8e3456e910208d684c8d35f3dd4ec0
Frank Carvalho <65919656+FrankSCarvalho@users.noreply.github.com>
2026-05-16 11:59:38 -0500

    Conclusão do nivel novato
    Conclui a primeira parte do desafio

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
+        Personagem heroi2 = new Personagem("Diana", "Mago", 2, 120, 15.8);
 
-        .
-        .
-        .
+
+        heroi1.exibirStatus();
+        heroi2.exibirStatus();
 
     }
 }
